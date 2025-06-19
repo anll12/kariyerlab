1b3c2046468e4ec1fdc0d9edb0cd65a07e2077d9
anll12 <anil.celik@tedu.edu.tr>
2025-06-19 21:04:28 +0300

    Yaptığın değişikliğe uygun bir mesaj

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,11 @@ const Contact = () => (
   <section id="contact" className="contact-section">
     <h2>İletişim</h2>
     <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }}>
-      Bize ulaşmak için: <a href="mailto:info@kariyerlab.com">info@kariyerlab.com</a>
+    <p>Bize ulaşmak için:</p>
+      <p><a href="mailto:anil.celik@tedu.edu.tr">anil.celik@tedu.edu.tr</a></p>
+      <p><a href="mailto:mustafa.karakus@tedu.edu.tr">mustafa.karakus@tedu.edu.tr</a></p>
+      <p><a href="mailto:a.hakan.aksoy@tedu.edu.tr">a.hakan.aksoy@tedu.edu.tr</a></p>
+
     </motion.p>
   </section>
 );
